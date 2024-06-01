aacdc8f088bbb9e053d8afd2844b429658a9c12c
Connor Todd <connortodd0304@gmail.com>
2024-06-01 12:53:01 +0100

    fetching of currency codes and rendering skeleton converter

--- src/components/CurrencyConverter/CurrencyConverter.js
+++ src/components/CurrencyConverter/CurrencyConverter.js
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
 export default function CurrencyConverter () {
+    const [currencyCodes, setCurrencyCodes] = useState([]);
+
+    useEffect(() => {
+        fetchCurrencyCodes();
+    }, []);
+
+    const fetchCurrencyCodes = () => {
+        fetch('https://blockchain.info/ticker')
+            .then(res => res.json())
+            .then(data => {
+                setCurrencyCodes(Object.keys(data))
+            })
+            .catch(error => console.log(error));
+    }
+
     return (
         <div>
         <h1>Currency to BTC Converter</h1>
+        <input type="number"  />
+        <select>
+            {currencyCodes.map(currencyCode => (
+                <option value={currencyCode}>{currencyCode}</option>
+            ))}
+        </select>
+        <button>Convert</button>
+
     </div>
     )
 }
