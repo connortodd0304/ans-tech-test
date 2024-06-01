662385ed64f23b88820a49e7768cd12a9f12f39e
Connor Todd <connortodd0304@gmail.com>
2024-06-01 13:06:01 +0100

    implemented currency to btc converter

--- src/components/CurrencyConverter/CurrencyConverter.js
+++ src/components/CurrencyConverter/CurrencyConverter.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 export default function CurrencyConverter () {
     const [currencyCodes, setCurrencyCodes] = useState([]);
+    const [selectedCurrencyCode, setSelectedCurrencyCode] = useState("ARS");
+    const [currentNumberValue, setCurrentNumberValue] = useState(0);
+    const [result, setResult] = useState(null);
 
     useEffect(() => {
         fetchCurrencyCodes();
@@ -16,17 +19,37 @@ export default function CurrencyConverter () {
             .catch(error => console.log(error));
     }
 
+    const convertCurrencyToBTC = () => {
+        fetch(`https://blockchain.info/tobtc?currency=${selectedCurrencyCode}&value=${currentNumberValue}`)
+        .then(res => res.json())
+        .then(data => {
+            setResult(
+                {
+                    bitcoinValue: data,
+                    currencyAmount: currentNumberValue,
+                    currencyCode: selectedCurrencyCode
+                }
+            )
+        })
+        .catch(error => console.log(error));
+    }
+
     return (
         <div>
         <h1>Currency to BTC Converter</h1>
-        <input type="number"  />
-        <select>
+        <input type="number" value={currentNumberValue} onChange={(event) => setCurrentNumberValue(event.target.value)}  />
+        <select onChange={(event) => setSelectedCurrencyCode(event.target.value)}>
             {currencyCodes.map(currencyCode => (
                 <option value={currencyCode}>{currencyCode}</option>
             ))}
         </select>
-        <button>Convert</button>
+        <button onClick={convertCurrencyToBTC}>Convert</button>
 
+        {result && (
+            <div>
+                {result.currencyAmount} {result.currencyCode} = {result.bitcoinValue} BTC
+            </div>
+        )}        
     </div>
     )
 }
